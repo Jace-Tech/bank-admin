93f9e0ee2a6c9c460c72b5fba30f7b917a6a98e3
Chidindu Emmanuel Aneke <86878828+Jace-Tech@users.noreply.github.com>
2023-03-13 22:53:57 +0100

    updated

--- inc/Sidebar.php
+++ inc/Sidebar.php
@@ -106,7 +106,7 @@
                 <ul class="pl-9 mt-4" :class="!open && 'hidden'" x-cloak>
                   <li class="mb-3 last:mb-0"><a class="block text-gray-400 hover:text-gray-200 transition duration-150 truncate" href="all-transactions"><span class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">All Transactions</span></a>
                   </li>
-                  <li class="mb-3 last:mb-0"><a class="block text-gray-400 hover:text-gray-200 transition duration-150 truncate" href="pending-transactions"><span class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Pending Transactions</span></a>
+                  <!-- <li class="mb-3 last:mb-0"><a class="block text-gray-400 hover:text-gray-200 transition duration-150 truncate" href="pending-transactions"><span class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Pending Transactions</span></a> -->
                   </li>
                   <!-- <li class="mb-3"><a class="block text-gray-400 hover:text-gray-200 transition duration-150 truncate" href="approved-transactions"><span class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Approved Transactions</span></a>
                   </li> -->
